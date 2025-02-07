d351f37529a4fdb23ecbdc6a4a5c93ab60948117
Lukas <lpetersonrhs@gmail.com>
2025-02-07 08:42:15 -0600

    upload promo reel

--- mentrek-epk-site/src/app/page.tsx
+++ mentrek-epk-site/src/app/page.tsx
@@ -29,12 +29,31 @@ export default function Home() {
         <div className="w-full">
           <iframe
             width="100%"
-            height="166"
+            height="300"
             scrolling="no"
             frameBorder="no"
             allow="autoplay"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1832719125&color=%23080404&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2028667856%3Fsecret_token%3Ds-LHhgz2MVvUs&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           ></iframe>
+          <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+            <a
+              href="https://soundcloud.com/mentrek_studio"
+              title="mentrek"
+              target="_blank"
+              style="color: #cccccc; text-decoration: none;"
+            >
+              mentrek
+            </a>{" "}
+            ·{" "}
+            <a
+              href="https://soundcloud.com/mentrek_studio/chapter-1/s-LHhgz2MVvUs"
+              title="chapter 1"
+              target="_blank"
+              style="color: #cccccc; text-decoration: none;"
+            >
+              chapter 1
+            </a>
+          </div>
         </div>
 
         {/* Artist Intro */}
